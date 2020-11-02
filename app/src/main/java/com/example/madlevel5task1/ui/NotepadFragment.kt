6f41476410fc9ed2ce2cac8bd60add01d6248f72
oscar <oscar.oosterling@hva.nl>
2020-11-02 22:09:39 +0100

    Step 4 completed, did item_note wrong

--- app/src/main/java/com/example/madlevel5task1/ui/NotepadFragment.kt
+++ app/src/main/java/com/example/madlevel5task1/ui/NotepadFragment.kt
@@ -5,12 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.example.madlevel5task1.R
+import com.example.madlevel5task1.model.NoteViewModel
+import kotlinx.android.synthetic.main.fragment_notepad.*
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class NotepadFragment : Fragment() {
+
+    private val viewModel:NoteViewModel by viewModels()
+
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -22,7 +30,15 @@ class NotepadFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+            observeAddNoteResult()
 
+    }
 
+    private fun observeAddNoteResult() {
+        viewModel.note.observe(viewLifecycleOwner, Observer { note->note?.let{
+            tvNoteTitle.text = it.title
+            txt_date_Notepadfragment.text = it.lastUpdated.toString()
+            tvNoteText.text = it.text
+        } })
     }
 }
